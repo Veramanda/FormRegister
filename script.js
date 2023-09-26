9217bf11372abb3afe8e54c9bf4de6dcaea28b8b
Veramanda <faidatun237@gmail.com>
2023-09-26 11:20:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let nilai = 80
+
+if (nilai >= 90 && nilai <=100) {
+    console.log("Nilai A")
+} else if (nilai >= 80) {
+    console.log("Nilai B") 
+} else if (nilai >= 70) {
+    console.log("Nilai C")
+} else if (nilai >= 0 && nilai ) {
+    console.log("Nilai D")
+} else {
+    console.log("Mungkin anda salah input")
+}
+
+// for looping
+for (i = 1; i >= 10; i++) {
+    console.log(i);
+}
